77a4fe42c075cc1bab68cfd734239607b9fbef96
lmarqu8 <46792388+lmarqu8@users.noreply.github.com>
2021-05-20 02:30:41 -0500

    sovled
    Runtime: 24 ms, faster than 96.48% of Python3 online submissions for Reverse Integer.
    Memory Usage: 14.2 MB, less than 72.29% of Python3 online submissions for Reverse Integer.

--- main.py
+++ main.py
@@ -2,28 +2,23 @@ from typing import List
 
 
 class Solution:
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
+    def reverse(self, x: int) -> int:
+        if x >= 2 ** 31 - 1: return 0
+        if x <= -2 ** 31: return 0
 
-        vals = []
-
-        size = len(nums)
-
-        for i in range(len(nums)):
-            for j in range(1, len(nums)):
-                if i != j:
-                    if target - nums[i] == nums[j]:
-                        vals.append(i)
-                        vals.append(j)
-
-            if len(vals) == 2: break
-
-        return vals
+        if x < 0:
+            y = y = str(x * -1)
+        else:
+            y = str(x)
+        y = y[::-1]
+        z = int(y)
+        if z > 2147483647: return 0
+        if z < -2147483648: return 0
+        if x < 0: z *= -1
+        return z
 
 
 if __name__ == '__main__':
     print("test")
 
-    nums = [2, 7, 11, 15]
-    target = 9
-
-    print(Solution.twoSum(Solution, nums, target))
+    print(Solution.reverse(Solution, 123))
