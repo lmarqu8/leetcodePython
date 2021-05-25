4145dd7593f24775e40dac66cb720591de025157
lmarqu8 <46792388+lmarqu8@users.noreply.github.com>
2021-05-24 20:16:25 -0500

    Solved
    Runtime: 40 ms, faster than 91.64% of Python3 online submissions for Roman to Integer.
    Memory Usage: 14.3 MB, less than 25.05% of Python3 online submissions for Roman to Integer.

--- main.py
+++ main.py
@@ -1,24 +1,65 @@
-from typing import List
-
-
 class Solution:
-    def reverse(self, x: int) -> int:
-        if x >= 2 ** 31 - 1: return 0
-        if x <= -2 ** 31: return 0
+    def romanToInt(self, s: str) -> int:
+        if s == "": return 0
 
-        if x < 0:
-            y = y = str(x * -1)
-        else:
-            y = str(x)
-        y = y[::-1]
-        z = int(y)
-        if z > 2147483647: return 0
-        if z < -2147483648: return 0
-        if x < 0: z *= -1
-        return z
+        if s[0] == "M":
+            if not s[1:]:
+                return 1000
+            else:
+                return 1000 + self.romanToInt(self, s[1:])
+
+        elif s[0] == "D":
+            return 500 + self.romanToInt(self, s[1:])
+
+        elif s[0] == "C":
+            if not s[1:]:
+                return 100
+
+            elif s[1] == "D":
+                return 400 + self.romanToInt(self, s[2:])
+
+            elif s[1] == "M":
+                return 900 + self.romanToInt(self, s[2:])
+
+            else:
+                return 100 + self.romanToInt(self, s[1:])
+
+        elif s[0] == "L":
+            return 50 + self.romanToInt(self, s[1:])
+
+        elif s[0] == "X":
+            if not s[1:]:
+                return 10
+
+            elif s[1] == "L":
+                return 40 + self.romanToInt(self, s[2:])
+
+            elif s[1] == "C":
+                return 90 + self.romanToInt(self, s[2:])
+
+            else:
+                return 10 + self.romanToInt(self, s[1:])
+
+        elif s[0] == "V":
+            return 5 + self.romanToInt(self, s[1:])
+
+        elif s[0] == "I":
+            if not s[1:]:
+                return 1
+
+            elif s[1] == "V":
+                return 4 + self.romanToInt(self, s[2:])
+
+            elif s[1] == "X":
+                return 9 + self.romanToInt(self, s[2:])
+
+            else:
+                return 1 + self.romanToInt(self, s[1:])
 
 
 if __name__ == '__main__':
-    print("test")
+    x = "test"
+    print(x)
+    print(x[1:])
 
-    print(Solution.reverse(Solution, 123))
+    print(Solution.romanToInt(Solution, "MMM"))
