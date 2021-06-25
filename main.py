cc31a5d41e75e58b78821157b143082dc6cc2e8e
lmarqu8 <46792388+lmarqu8@users.noreply.github.com>
2021-06-24 20:38:17 -0500

    Details Runtime: 32 ms, faster than 81.03% of Python3 online submissions for Longest Common Prefix. Memory Usage: 14.2 MB, less than 80.10% of Python3 online submissions for Longest Common Prefix.

--- main.py
+++ main.py
@@ -1,16 +1,18 @@
-# This is a sample Python script.
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
 
-# Press Shift+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+        strs.sort(key=len)
+        if len(strs[0]) == 0: return ""
+        lcp = ""
+        temp = strs[0][0]
+        size = len(strs)
+        lettercount = len(strs[0])
 
-
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
-
-
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+        for x in range(lettercount):
+            for word in strs:
+                if (temp[x] != word[x]):
+                    return lcp
+            lcp = temp
+            if (x == lettercount - 1): return lcp
+            temp += word[x + 1]
+        return lcp
